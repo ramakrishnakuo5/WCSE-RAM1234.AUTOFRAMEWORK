e6883f53de10bf471304ae3d11b6da9b3352d9cf
user <user@DESKTOP-BIPJ7KQ>
2023-03-02 14:28:36 +0530

    Framework pushed

---
+++ src/test/java/vTiger/Practice/CalendarPopUp.java
@@ -0,0 +1,16 @@
+package vTiger.Practice;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class CalendarPopUp {
+	
+@Test
+public void calendarPopUp()
+{
+WebDriver driver =new ChromeDriver();
+driver.get("https://www.makemytrip.com/");
+}
+
+}
